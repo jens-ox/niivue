c824a25964454f3a290a1ca8d32a495f78b12447
Jens Ochsenmeier <github@jens-ox.de>
2023-11-28 12:39:40 +0100

    [ts] nvlabel

--- src/nvlabel.ts
+++ src/nvlabel.ts
@@ -1,39 +1,50 @@
+import { ColorArray } from './types.js'
+
 /** enum for text alignment */
-export const LabelTextAlignment = Object.freeze({
-  LEFT: 'left',
-  RIGHT: 'right',
-  CENTER: 'center'
-})
+export enum LabelTextAlignment {
+  LEFT = 'left',
+  RIGHT = 'right',
+  CENTER = 'center'
+}
 
 /** enum for line terminators */
-export const LabelLineTerminator = Object.freeze({
-  NONE: 'none',
-  CIRCLE: 'circle',
-  RING: 'ring'
-})
+export enum LabelLineTerminator {
+  NONE = 'none',
+  CIRCLE = 'circle',
+  RING = 'ring'
+}
 
-/**  Class representing label style */
+/**
+ * Class representing label style
+ * @param textColor Color of text
+ * @param textScale Text Size (0.0..1.0)
+ * @param lineWidth Line width
+ * @param lineColor Line color
+ * @param bulletScale Bullet size respective of text
+ * @param bulletColor Bullet color
+ * @param backgroundColor Background color of label
+ */
 export class NVLabel3DStyle {
-  /**
-   *
-   * @param {number[]} textColor - Color of text
-   * @param {number} textScale - Text Size (0.0..1.0)
-   * @param {number} lineWidth - Line width
-   * @param {number[]} lineColor - Line color
-   * @param {number} bulletScale - Bullet size respective of text
-   * @param {number[]} bulletColor - Bullet color
-   * @param {number[]} backgroundColor - Background color of label
-   */
+  textColor: ColorArray
+  textScale: number
+  textAlignment: LabelTextAlignment
+  lineWidth: number
+  lineColor: ColorArray
+  lineTerminator: LabelLineTerminator
+  bulletScale?: number
+  bulletColor?: ColorArray
+  backgroundColor?: ColorArray
+
   constructor(
-    textColor = [1.0, 1.0, 1.0, 1.0],
+    textColor: ColorArray = [1.0, 1.0, 1.0, 1.0],
     textScale = 1.0,
     textAlignment = LabelTextAlignment.LEFT,
     lineWidth = 0.0,
-    lineColor = [0.0, 0.0, 0.0],
+    lineColor: ColorArray = [0.0, 0.0, 0.0],
     lineTerminator = LabelLineTerminator.NONE,
-    bulletScale = undefined,
-    bulletColor = undefined,
-    backgroundColor = undefined
+    bulletScale?: number,
+    bulletColor?: ColorArray,
+    backgroundColor?: ColorArray
   ) {
     this.textColor = textColor
     this.textScale = textScale
@@ -47,15 +58,19 @@ export class NVLabel3DStyle {
   }
 }
 
+/**
+ * Label class
+ * @constructor
+ * @param text The text of the label
+ * @param style The style of the label
+ * @param points An array of points label for label lines
+ */
 export class NVLabel3D {
-  /**
-   * Label class
-   * @constructor
-   * @param {string} text - The text of the label
-   * @param {NVLabel3DStyle} style - The style of the label
-   * @param {number[][]} points - An array of points label for label lines
-   */
-  constructor(text, style, points) {
+  text: string
+  style: NVLabel3DStyle
+  points: number[][]
+
+  constructor(text: string, style: NVLabel3DStyle, points: number[][]) {
     this.text = text
     this.style = style
     this.points = points
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type ColorArray = [number, number, number] | [number, number, number, number]
